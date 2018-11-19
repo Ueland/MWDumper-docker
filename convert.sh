f179b5d8e114b877f9b707a8d24315529485dc0b
Adam Vollrath <adam@endpoint.com>
2018-11-19 17:43:13 -0600

    Clean up `mysql` options.

--- convert.sh
+++ convert.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl $(DUMP_URL) | java -jar /app/mwdumper.jar --format=sql:1.25 - | mysql -u $(DB_USER) -D $(DB_DATABASE) -h $(DB_HOST) -p $(DB_PASSWORD)
+curl $(DUMP_URL) | java -jar /app/mwdumper.jar --format=sql:1.25 - | mysql --user=$(DB_USER) --database=$(DB_DATABASE) -h $(DB_HOST) --password=$(DB_PASSWORD)
--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -jar /app/mwdumper.jar --format=sql:1.25 $1 > /sql/$1.sql
