dfce2c9ecd10032171e94d484590a898b74c7346
Adam Vollrath <adam@endpoint.com>
2018-11-19 19:30:33 -0600

    inline bunzip2

--- convert.sh
+++ convert.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl "$DUMP_URL" | java -jar /app/mwdumper.jar --format=sql:1.25 - | mysql --user=$DB_USER --database=$DB_DATABASE -h $DB_HOST --password=$DB_PASSWORD
+curl "$DUMP_URL" | bunzip2 | java -jar /app/mwdumper.jar --format=sql:1.25 - | mysql --user=$MYSQL_USER --database=$MYSQL_DATABASE -h $MYSQL_HOST --password=$MYSQL_PASSWORD
