77d9dec3298b183f60dcf1ff5e2ff86d2912405f
Adam Vollrath <adam@endpoint.com>
2018-11-28 14:26:38 -0600

    Retry `curl` HTTP fetches.

--- convert.sh
+++ convert.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl --silent --show-error "$DUMP_URL" | \
+curl --retry 10000 --silent --show-error "$DUMP_URL" | \
 	bunzip2 | \
 	java -jar /app/mwdumper.jar --format=sql:1.25 - | \
 	mysql --force --user=$MYSQL_USER --database=$MYSQL_DATABASE --host=$MYSQL_HOST --password=$MYSQL_PASSWORD
