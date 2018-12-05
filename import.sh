ccb5ff2948aa90f7e6cf6d131cc0177f058f8bdc
Adam Vollrath <adam@endpoint.com>
2018-12-05 11:45:42 -0600

    Specify MySQL host and fix typo in import shell script.

--- import.sh
+++ import.sh
@@ -1,5 +1,5 @@
-for url in `cat manifest.txt`:
+for url in `cat manifest.txt`
 do
 	echo "docker run --rm --name mwdumper-$(echo $url | cut -d'-' -f6 | cut -f1 -d'.') -e MYSQL_HOST=${MYSQL_HOST} -e MYSQL_PASSWORD=${MYSQL_PASSWORD} -e DUMP_URL=\"$url\" axisofentropy/mwdumper-docker"
-	docker run --rm --name mwdumper-$(echo $url | cut -d'-' -f6 | cut -f1 -d'.') -e MYSQL_HOST=${MYSQL_HOST} -e MYSQL_PASSWORD=${MYSQL_PASSWORD} -e DUMP_URL="$url" axisofentropy/mwdumper-docker
+	docker run --rm --name mwdumper-$(echo $url | cut -d'-' -f6 | cut -f1 -d'.') -e MYSQL_DATABASE="${MYSQL_DATABASE}" -e MYSQL_HOST=${MYSQL_HOST} -e MYSQL_PASSWORD=${MYSQL_PASSWORD} -e DUMP_URL="$url" axisofentropy/mwdumper-docker
 done
